429d115db05e9e74f6775368e979edcf3f16333e
LayKim2 <102665441+LayKim2@users.noreply.github.com>
2025-01-03 19:20:43 +0900

    [chore] default DB setting

--- HashKorea-Blazor/Program.cs
+++ HashKorea-Blazor/Program.cs
@@ -1,4 +1,12 @@
+using CalyxManagement.Data;
+using DotNetEnv;
 using HashKorea_Blazor.Components;
+using Microsoft.EntityFrameworkCore;
+
+var solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.FullName;
+var envPath = Path.Combine(solutionDirectory, ".env");
+
+Env.Load(envPath);
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +19,28 @@ builder.Services.AddRazorComponents()
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
+
+
+// DBContext
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    // 1. MSSQL
+    // options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+    // 2. MYSQL(MariaDB)
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+    //var connectionString = string.Format(
+    //    builder.Configuration.GetConnectionString("DefaultConnection"),
+    //    Environment.GetEnvironmentVariable("AWS_SERVER"),
+    //    Environment.GetEnvironmentVariable("AWS_DATABASE"),
+    //    Environment.GetEnvironmentVariable("AWS_USER"),
+    //    Environment.GetEnvironmentVariable("AWS_PASSWORD")
+    //);
+
+    //options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
