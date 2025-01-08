eaceb8dc395ab28b2bd01bda0a9a667d534b624f
LayKim2 <102665441+LayKim2@users.noreply.github.com>
2025-01-08 18:01:04 +0900

    [feature] image lazy

--- HashKorea-Blazor/wwwroot/js/common.js
+++ HashKorea-Blazor/wwwroot/js/common.js
@@ -1,1 +1,16 @@
-﻿
+﻿window.initLazyLoading = function () {
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                img.src = img.dataset.src;
+                img.classList.remove('lazy-image');
+                observer.unobserve(img);
+            }
+        });
+    });
+
+    document.querySelectorAll('img.lazy-image').forEach(img => {
+        observer.observe(img);
+    });
+}
